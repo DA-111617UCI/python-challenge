cbbb09eb22a2983bd85f0c1173ee8c0c516b6cad
DA-111617UCI <33743327+DA-111617UCI@users.noreply.github.com>
2018-03-02 14:53:52 -0800

    Update HW files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,94 +1,61 @@
-# PyBank
-# Revenue
-# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will be given two sets of revenue data (budget_data_1.csv and budget_data_2.csv). Each dataset is composed of two columns: Date and Revenue. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
-
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-
-# The total number of months included in the dataset
-
-# The total amount of revenue gained over the entire period
-
-# The average change in revenue between months over the entire period
-
-# The greatest increase in revenue (date and amount) over the entire period
-
-# The greatest decrease in revenue (date and amount) over the entire period
-
-# As an example, your analysis should look similar to the one below:
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 25
-# Total Revenue: $1241412
-# Average Revenue Change: $216825
-# Greatest Increase in Revenue: Sep-16 ($815531)
-# Greatest Decrease in Revenue: Aug-12 ($-652794)
-# Your final script must be able to handle any such similarly structured dataset in the future (your boss is going to give you more of these -- so your script has to work for the ones to come). In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Dependencies
-import os
 import csv
-import pandas as pd
-import moment
-from datetime import datetime
-import random
 
+# Files to load and output (Remember to change these)
+file_to_load = "raw_data/budget_data_1.csv"
+file_to_output = "analysis/budget_analysis_1.txt"
 
-# Create output file name
-file_output_name = "budget_data_output.csv"
-file_input_name = input("Enter file name: " ) + ".csv"
-bank_csvpath = os.path.join(file_input_name)
+# Track various revenue parameters
+total_months = 0
+prev_revenue = 0
+month_of_change = []
+revenue_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_revenue = 0
 
-# Create DF from Pandas
-df = pd.read_csv(bank_csvpath)
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as revenue_data:
+    reader = csv.DictReader(revenue_data)
 
-# Find the range of Revenue [lowest, highest]
+    for row in reader:
 
-# Maximum Values
-max_revenue = max(df.iloc[:,1])
-max_index = df.loc[df['Revenue']==max_revenue].index
-max_date = df.iloc[max_index, 0]
+        # Track the total
+        total_months += 1
+        total_revenue = total_revenue + int(row["Revenue"])
 
-# Minimum Values
-min_revenue = min(df.iloc[:,1])
-min_index = df.loc[df['Revenue']==min_revenue].index
-min_date = df.iloc[min_index, 0]
+        # Track the revenue change
+        revenue_change = int(row["Revenue"]) - prev_revenue
+        prev_revenue = int(row["Revenue"])
+        revenue_change_list.append(revenue_change)
+        month_of_change.append(row["Date"])
 
-# Total Revenue
-tot_revenue = sum(df.iloc[:,1])
-# List to store change in revenues over the period
-Avg_Range = []
-# Open the file
-with open(bank_csvpath, newline = "", encoding = 'utf-8') as bankfile:
-    bank_reader = csv.reader(bankfile, delimiter = ",")
-    neg_counter = 0
-    # Skip header line
-    next(bank_reader, None)
+        # Calculate the greatest increase
+        if (revenue_change > greatest_increase[1]):
+            greatest_increase[0] = row["Date"]
+            greatest_increase[1] = revenue_change
 
-# Calculate average rate of change in Revenue
-    tot_months = len(list(bank_reader)) + 1
-    for i  in range(1,tot_months-2):
-        Rev_change = (df.iloc[i+1, 1] - df.iloc[i, 1])
-        Avg_Range.append(Rev_change)
-    Avg_ROC = (sum(Avg_Range))/(tot_months-1)
-    ChangeInRevenue = Avg_ROC
+        # Calculate the greatest decrease
+        if (revenue_change < greatest_decrease[1]):
+            greatest_decrease[0] = row["Date"]
+            greatest_decrease[1] = revenue_change
 
-# Range of profits or losses (Chane as needed)
-range_revenue = [int(min_revenue), int(max_revenue)]
+# Calculate the Average Revenue Change
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
 
-print("FINANCIAL ANALYSIS")
-print("-----------------------------------")
-print("Total Months: " + str(tot_months))
-print("Total Revenue: " + "${:.2f}".format(tot_revenue))
-print("Average Revenue Change: " + "${:.2f}".format(ChangeInRevenue))
-print("Greatest Increase in Revenue: " + max_date + " " + "${:,}".format(max_revenue))
-print("Greatest Decrease in Revenue: " + min_date + " " + "${:,}".format(min_revenue))
-print("--------------------------------------------------------")
-# print(max_revenue)
-# print(min_revenue)
-# print(min_date)
-# print(max_date)
-# print(Avg_Range)
+# Generate Output Summary
+output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total Revenue: ${total_revenue}\n"
+    f"Average Revenue Change: ${revenue_avg}\n"
+    f"Greatest Increase in Revenue: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
-with open("Output.txt", "w") as text_file:
-    print("FINANCIAL ANALYSIS\n -----------------------------------\nTotal Months: " + str(tot_months) + "\nTotal Revenue: " + "${:.2f}".format(tot_revenue) + "\nAverage Revenue Change: " + "${:.2f}".format(ChangeInRevenue) + "\nGreatest Increase in Revenue: " + max_date + " " + "${:,}".format(max_revenue) + "\nGreatest Decrease in Revenue: " + min_date + " " + "${:,}".format(min_revenue) + "\n--------------------------------------------------------\n"    , file=text_file)
+# Print the output (to terminal)
+print(output)
+
+# Export the results to text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,49 +1,119 @@
-# -*- coding: UTF-8 -*-
-"""Generate Data for PyBoss.
-
-This module generates an employee dataset for the PyBoss Homework.
-"""
-# Dependencies
+# Import required packages
 import csv
-import random
-from faker import Factory
+import datetime
 
-# Fake data generator
-fake = Factory.create("en_US")
+# Files to load and output (Remember to change these)
+file_to_load = "raw_data/employee_data2.csv"
+file_to_output = "analysis/employee_data_reformatted2.csv"
 
-# Create output file name
-file_output_name = "employee_data1.csv"
+# Dictionary of states with abbreviations
+us_states = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+}
 
-# Set employee counts
-emp_count = 650
+# Placeholders for re-formatted contents
+employee_ids = []
+employee_first_names = []
+employee_last_names = []
+employee_dobs = []
+employee_ssns = []
+employee_states = []
 
-# Create emp_ids
-emp_ids = random.sample(range(1, 655), emp_count)
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as emp_data:
+    reader = csv.DictReader(emp_data)
 
+    # Loop through each row, re-grab each field and store in a new list
+    for row in reader:
 
-# Create variety of fake employee data
-def get_dob():
-    """Generate a fake date-of-birth."""
-    return (
-        fake.date_time_between(
-            start_date="-45y",
-            end_date="-22y",
-            tzinfo=None)).strftime("%Y-%m-%d")
+        # Grab emp_ids and store it into a list
+        employee_ids = employee_ids + [row["Emp ID"]]
 
-emp_names = ["{} {}".format(fake.first_name(), fake.last_name()) for _ in range(emp_count)]
-emp_dobs = [get_dob() for _ in range(emp_count)]
-emp_ssns = [fake.ssn() for _ in range(emp_count)]
-emp_states = [fake.state() for _ in range(emp_count)]
+        # Grab names, split them, and store them in a temporary variable
+        split_name = row["Name"].split(" ")
 
-# Combine all data into a single list
-emp_db = zip(emp_ids, emp_names, emp_dobs, emp_ssns, emp_states)
+        # Then save first and last name in separate lists
+        employee_first_names = employee_first_names + [split_name[0]]
+        employee_last_names = employee_last_names + [split_name[1]]
 
-# Print the employee list to terminal
-print(emp_db)
+        # Grab DOB and reformat it
+        reformatted_dob = datetime.datetime.strptime(row["DOB"], "%Y-%m-%d")
+        reformatted_dob = reformatted_dob.strftime("%m/%d/%Y")
 
-# Output the employee list to a csv
+        # Then store it into a list
+        employee_dobs = employee_dobs + [reformatted_dob]
+
+        # Grab SSN and reformat it
+        split_ssn = list(row["SSN"])
+        split_ssn[0:3] = ("*", "*", "*")
+        split_ssn[4:6] = ("*", "*")
+        joined_ssn = "".join(split_ssn)
+
+        # Then store it into a list
+        employee_ssns = employee_ssns + [joined_ssn]
+
+        # Grab the states and use the dictionary to find the replacement
+        state_abbrev = us_states[row["State"]]
+
+        # Then store the abbreviation into a list
+        employee_states = employee_states + [state_abbrev]
+
+# Zip all of the new lists together
+empdb = zip(employee_ids, employee_first_names, employee_last_names,
+            employee_dobs, employee_ssns, employee_states)
+
 # Write all of the election data to csv
-with open(file_output_name, "w", newline="") as datafile:
+with open(file_to_output, "w", newline="") as datafile:
     writer = csv.writer(datafile)
-    writer.writerow(["Emp ID", "Name", "DOB", "SSN", "State"])
-    writer.writerows(emp_db)
+    writer.writerow(["Emp ID", "First Name", "Last Name",
+                     "DOB", "SSN", "State"])
+    writer.writerows(empdb)
